5f63138a3f4ba11ca9814b9f8da8d01103bfaf43
tommypurcell <tommypurcelljr@gmail.com>
2024-02-25 16:05:06 +0700

    style changes on blog page and functioning nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -9,13 +9,33 @@
 (function buttonControlInit() {
   const navToggle = document.querySelector("#nav_toggle");
   const navContent = document.querySelector("#nav_content");
+  const homeNavContent = document.querySelector("#home_nav_content")
   const nav = document.querySelector("nav");
 
   function toggleNav() {
-    navContent.classList.toggle("hidden");
-    navContent.classList.toggle("mobile-nav-background")
+    // Check if navContent exists before toggling class
+    if (navContent) {
+      navContent.classList.toggle("hidden");
+    } else {
+      console.error("navContent element not found!");
+    }
 
+    // Assuming homeNavContent might be null in some pages
+    if (homeNavContent) {
+      homeNavContent.classList.toggle("mobile-nav-background");
+      homeNavContent.classList.toggle("hidden");
+    } else {
+      console.error("homeNavContent element not found!");
+    }
   }
+
+  // Bind the toggleNav function to the click event of navToggle if navToggle exists
+  if (navToggle) {
+    navToggle.addEventListener('click', toggleNav);
+  } else {
+    console.error("navToggle button not found!");
+  }
+
 
   navToggle.addEventListener("click", toggleNav);
 
