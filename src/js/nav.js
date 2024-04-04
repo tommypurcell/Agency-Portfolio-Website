dba2ff0d89e9ca166f645893466c8cf16d95047f
tommypurcell <tommypurcelljr@gmail.com>
2024-04-04 14:28:38 +0800

    Code format auto eslint

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,27 +7,24 @@
 // 4. Add click listener to navToggle to call toggle() on navContent when navToggle is clicked
 
 (function buttonControlInit() {
-  const navToggle = document.querySelector("#nav_toggle");
-  const navContent = document.querySelector("#nav_content");
-  const nav = document.querySelector("nav");
+  const navToggle = document.querySelector('#nav_toggle')
+  const navContent = document.querySelector('#nav_content')
 
   function toggleNav() {
-    navContent.classList.toggle("hidden");
-
-
+    navContent.classList.toggle('hidden')
   }
 
-  navToggle.addEventListener("click", toggleNav);
+  navToggle.addEventListener('click', toggleNav)
 
   // FAQ Dropdown Control
   // 1. Gather all <detail> elements into an array
   // 2. Assign a click listener to each one
   // 3. Click listener toggles "open" class when clicked
 
-  const detailsArray = document.querySelectorAll("details");
+  const detailsArray = document.querySelectorAll('details')
 
   function toggleDetails(e) {
-    e.target.classList.toggle("open");
+    e.target.classList.toggle('open')
   }
-  detailsArray.forEach((el) => el.addEventListener("toggle", toggleDetails));
-})();
+  detailsArray.forEach((el) => el.addEventListener('toggle', toggleDetails))
+})()
